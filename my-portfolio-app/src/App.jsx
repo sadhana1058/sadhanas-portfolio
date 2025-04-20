eff5d2d0146984430d2097d44618ff612f00bb83
sadhana1058 <sadhana1058@gmail.com>
2025-04-20 08:55:35 -0400

    basic set up done

--- my-portfolio-app/src/App.jsx
+++ my-portfolio-app/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+// import {About} from '../src/container/index'
+// About/About.jsx'
+import {About,Footer,Header,Skills,Testimonial,Work}from '../src/container/index'
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
      <h1>Hello World Sadhana Here</h1>
+     <Header/>
+     <About/>
+     <Work/>
+     <Skills/>
+     <Testimonial/>
+     <Footer/>
     </div>
   )
 }
